fe731ee4ca9ff1fa0de582a0b0f7af1217cfd7b3
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-07-24 21:58:46 +0800

    Introduce XeniumOC

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Xenium Kernel - #LoveXe
+kernel.string=XeniumOC Kernel - #LoveXe
 do.devicecheck=1
 do.modules=0
 do.systemless=1
