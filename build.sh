fe360e308cdfcb5087dd2a4d8965b3bd445d119a
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-08-20 12:33:14 +0800

    Introduce DCLICK Variant

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 	export MAIN=`readlink -f ../`
 	export KERNELDIR=`readlink -f .`
 	
-	# LV Version
+	# LV Version + TPD
 	rm $KERNELDIR/built/Image.gz-dtb
 	make ARCH=arm64 X00TD_defconfig
 	make -j$(nproc --all) ARCH=arm64 \
@@ -18,10 +18,27 @@
 	echo "LV Compile Done"
 	echo ""
 	cd $KERNELDIR/built
-	zip -r Overdose-X00TD-`date +%Y%m%d_%H%M`.zip * -x "*.zip" "nlv.patch"
+	zip -r Overdose-X00TD-`date +%Y%m%d_%H%M`-TPD.zip * -x "*.zip" "nlv.patch" "dclick.patch"
 	cd $KERNELDIR/
+	
+	# LV Version + DCLICK
+	git am $KERNELDIR/built/dclick.patch
+	rm $KERNELDIR/built/Image.gz-dtb
+	make ARCH=arm64 X00TD_defconfig
+	make -j$(nproc --all) ARCH=arm64 \
+                      CC="$MAIN/proton-clang/bin/clang" \
+                      CROSS_COMPILE="$MAIN/proton-clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="$MAIN/proton-clang/bin/arm-linux-gnueabi-"
+	mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/built/Image.gz-dtb
+	echo ""
+	echo "LV Compile Done"
+	echo ""
+	cd $KERNELDIR/built
+	zip -r Overdose-X00TD-`date +%Y%m%d_%H%M`-DCLICK.zip * -x "*.zip" "nlv.patch" "dclick.patch"
+	cd $KERNELDIR/
+	git reset --hard HEAD^
 
-	# NLV Version
+	# NLV Version + TPD
 	git am $KERNELDIR/built/nlv.patch
 	rm $KERNELDIR/built/Image.gz-dtb
 	make ARCH=arm64 X00TD_defconfig
@@ -34,7 +51,24 @@
 	echo "NLV Compile Done"
 	echo ""
 	cd $KERNELDIR/built
-	zip -r Overdose-X00TD-`date +%Y%m%d_%H%M`-NLV.zip * -x "*.zip" "nlv.patch"
+	zip -r Overdose-X00TD-`date +%Y%m%d_%H%M`-NLV-TPD.zip * -x "*.zip" "nlv.patch" "dclick.patch"
 	cd $KERNELDIR/
+	
+	# NLV Version + TPD
+	git am $KERNELDIR/built/dclick.patch
+	rm $KERNELDIR/built/Image.gz-dtb
+	make ARCH=arm64 X00TD_defconfig
+	make -j$(nproc --all) ARCH=arm64 \
+                      CC="$MAIN/proton-clang/bin/clang" \
+                      CROSS_COMPILE="$MAIN/proton-clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="$MAIN/proton-clang/bin/arm-linux-gnueabi-"
+	mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/built/Image.gz-dtb
+	echo ""
+	echo "NLV Compile Done"
+	echo ""
+	cd $KERNELDIR/built
+	zip -r Overdose-X00TD-`date +%Y%m%d_%H%M`-NLV-DCLICK.zip * -x "*.zip" "nlv.patch" "dclick.patch"
+	cd $KERNELDIR/
+	git reset --hard HEAD^
 	git reset --hard HEAD^
 	echo "Zip Done"
